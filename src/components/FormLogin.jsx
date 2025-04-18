b31d750b45453163b5ba2628af77738ed2ed74b6
Kayque-Cassiano <kayque.cassiano1@gmail.com>
2025-04-17 21:09:13 -0300

    resolvendo os caminhos dos links

--- src/components/FormLogin.jsx
+++ src/components/FormLogin.jsx
@@ -1,10 +1,26 @@
-import React from 'react'
-import Input from "../components/input.jsx"
-import ButtonLogin from "../components/ButtonLogin.jsx"
-import '../styles/formLogin.css'
-import { Link } from 'react-router-dom'
+import React, { useState } from 'react';
+import Input from "../components/input.jsx";
+import ButtonLogin from "../components/ButtonLogin.jsx";
+import '../styles/formLogin.css';
+import { Link, useNavigate} from 'react-router-dom';
+import NProgress from 'nprogress';
+import 'nprogress/nprogress.css';
+
 
 function FormLogin() {
+    const [isFading, setIsFading] = useState(false);
+    const navigate = useNavigate();
+
+    const handleClick = (path, event) => {
+        event.preventDefault();
+        setIsFading(true);
+        NProgress.start();
+        setTimeout(() => {
+            navigate(path);
+            NProgress.done();
+        }, 1000); // atraso em milissegundos
+    };
+
     return (
         <form action="#" method="post" className='form-login'>
             <div className='text-content'>
@@ -24,16 +40,16 @@ function FormLogin() {
                     id="senha" />
             </div>
             <div className='link'>
-                <Link href={"/Home"}>Esqueceu a Senha?</Link>
+                <Link onClick={(event) => handleClick("/", event)}>Esqueceu a Senha?</Link>
             </div>
-            <ButtonLogin className="button" type="submit" value="Entrar" />
-            <span className='text-content'>Não possui uma conta?
-                <span className='link'>
-                    <Link to={"/Cadastro"}> Crie uma conta</Link>
+            <ButtonLogin onClick={(event) => handleClick("../", event)} className="button" type="submit" value="Entrar" />
+            <span className='text-content font-size'>Não possui uma conta?
+                <span className='link font-size'>
+                    <Link onClick={(event) => handleClick('/Cadastro', event)}> Crie uma conta</Link>
                 </span>
             </span>
         </form>
-    )
+    );
 }
 
-export default FormLogin
+export default FormLogin;
